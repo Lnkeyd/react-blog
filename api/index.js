9847ff224bf7b76177aee7f1872b32c2ede8b408
Nmgix <frostdraky@gmail.com>
2022-05-21 21:40:17 +0300

    minor changes

--- api/index.js
+++ api/index.js
@@ -39,7 +39,11 @@ app.use("/api/categories", categoryRoute);
 if (process.env.NODE_ENV === "production") {
   // Exprees will serve up production assets
   app.use(express.static("../client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+  });
 }
+
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend is running...");
 });

6b39b4f5dbb917463e137a1e9313f594444a720b
Nmgix <frostdraky@gmail.com>
2022-05-21 21:48:24 +0300

    minor changes

--- api/index.js
+++ api/index.js
@@ -38,7 +38,7 @@ app.use("/api/categories", categoryRoute);
 
 if (process.env.NODE_ENV === "production") {
   // Exprees will serve up production assets
-  app.use(express.static("../client/build"));
+  app.use(express.static(path.resolve(__dirname, "../client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
   });
